fd25280f390be7ba254b35592033fff41beb5aff
CodingAddict1530 <kejack2001@gmail.com>
2024-07-18 16:57:24 -0400

    Added icons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,12 @@ module com.project.javaeditor {
     requires javafx.web;
     requires javafx.swing;
     requires org.fxmisc.richtext;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
 
