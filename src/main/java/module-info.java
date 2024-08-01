950db4f72cff888ab602c947e21e6127445ce3ad
CodingAddict1530 <kejack2001@gmail.com>
2024-08-01 18:05:23 -0400

    Updated dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,21 +1,15 @@
 module com.project.javaeditor {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
     requires javafx.swing;
     requires org.fxmisc.richtext;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.ikonli.fontawesome5;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
     requires java.compiler;
     requires org.apache.logging.log4j.core;
     requires org.eclipse.lsp4j;
     requires org.eclipse.lsp4j.jsonrpc;
     requires com.google.gson;
+    requires org.fxmisc.undo;
 
     opens com.project.javaeditor to javafx.fxml;
     opens com.project.managers to org.eclipse.lsp4j.jsonrpc;
