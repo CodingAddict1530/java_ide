ef4316cd0d24b5d13ae85b33c5f5f35dfbd23733
CodingAddict1530 <kejack2001@gmail.com>
2024-08-20 18:07:38 -0400

    Added new class

---
+++ src/main/java/com/project/javaeditor/EntryPoint.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2024 Alexis Mugisha
+ * https://github.com/CodingAddict1530
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.project.javaeditor;
+
+/**
+ * This is the beginning point of the application.
+ * This is to be able to start the app as a native application since it does not extend javafx.application.Application.
+ */
+public class EntryPoint {
+
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+
+}
